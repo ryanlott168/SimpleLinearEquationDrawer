33e04c0006ef9731c55bd45cbd3f6185470d54df
toranlyt <toranlyt@gmail.com>
2017-06-17 00:54:59 -0500

    Add files via upload

--- line.js
+++ line.js
@@ -1,20 +1,34 @@
-
-var solveLinEq = {
+/*
+*-------------------------------------------------------------------------
+*   Hopefully this program satisfies what you were asking for.
+*   It can take simple linear equations in the form of 'y = mx + b'.
+*   As these lines weren't going to be on a graph, I didn't pay attention
+*   to graphing their 'y-intercept' or 'b'.
+*
+*   To use the object, just use the 'graphEquation()' function in a browser.
+*
+*   I hope you like it!
+*
+*   Best,
+*   Ryan Lott
+*--------------------------------------------------------------------------
+*/
+let solveLinEq = {
   // Declaring x, y, and slope (m) variables
   y: 0, x: 0, m: "",
   // Declaring slope indices within string of
   mStart: 0, mEnd: 0,
 
    // Remove any potential whitespace characters
-  removeSpaces: function(equation){
+  removeSpaces(equation){
     return equation = equation.replace(/\s/g,'');
    },
 
    // For loop to iterate through equation string to procure slope indices
-   getSlope: function(equation){
+   getSlope(equation){
 
      // Gets start and stop of slope
-    for(var index = 0; index < equation.length; ++index){
+    for(let index = 0; index < equation.length; ++index){
       if(equation[index] == "="){
         mStart = index + 1;
         // If slope isn't present, assume slope is 1
@@ -40,13 +54,13 @@ var solveLinEq = {
      }else{
        // Takes substring that includes slope
        m = equation.slice(mStart, mEnd + 1);
-       var xStart = 0, xEnd = mEnd-2, yStart = mStart-2, yEnd = 0;
+       let xStart = 0, yEnd = 0;
+       const xEnd = mEnd-2, yStart = mStart-2
        // Check for fraction
        if(m.includes("/")){
          // If contains fraction
          yEnd = m.indexOf("/") - 1;
          xStart = m.indexOf("/") + 1;
-         console.log(m);
 
          // If x and y are both single digits, can return slope
          if(yEnd == yStart && xEnd == xStart){
@@ -66,12 +80,12 @@ var solveLinEq = {
      }
    },
 
-   drawLine: function(slope){
-     var newLine = "";
+   drawLine(slope){
+     let newLine = "";
 
      // Check signs to know where line should start
      if((slope[0] > 0 && slope[1] > 0) || (slope[0] < 0 && slope[1] < 0)){
-       var spaces = "                                                  ";
+       let spaces = "                                                  ";
        // If slope is positive is both y and x are both negative
        slope[0] = Math.abs(slope[0]), slope[1] = Math.abs(slope[1]);
        // Calculates number of '\n' based on y
@@ -79,8 +93,8 @@ var solveLinEq = {
          newLine += "\n";
        }
        // Prints 10 stars (or less), removes spaces based on x value
-       for(var z = 10; z > 0; --z){
-           console.log(spaces + "*" + newLine);
+       for(let z = 10; z > 0; --z){
+           console.log(`${spaces}*${newLine}`);
            spaces = spaces.slice(0, -slope[1]);
            // Breaks if runs out of space
            if(spaces.length == 0){
@@ -88,8 +102,8 @@ var solveLinEq = {
            }
         }
       }else{
-        var spaces = "";
-        var spacesToAdd = "";
+        let spaces = "";
+        let spacesToAdd = "";
         // If slope is negative is either y or x are negative
         slope[0] = Math.abs(slope[0]), slope[1] = Math.abs(slope[1]);
         // Calculates number of '\n' based on y
@@ -101,8 +115,8 @@ var solveLinEq = {
           spacesToAdd += " ";
         }
         // Prints 10 stars (or less), removes spaces based on x value
-        for(var z = 10; z > 0; --z){
-            console.log(spaces + "*" + newLine);
+        for(let z = 10; z > 0; --z){
+            console.log(`${spaces}*${newLine}`);
             spaces += spacesToAdd;
             // Breaks if runs out of space
             if(spaces.length  >= 50){
@@ -112,12 +126,12 @@ var solveLinEq = {
       }
     },
 
-    graphEquation: function(){
+    graphEquation(){
       // This function allows you to take advantage of the previous functions
-      var input = window.prompt("Hello!\nPlease input a simple function: ");
+      let input = window.prompt("Hello!\nPlease input a simple function: ");
       input = this.removeSpaces(input);
-      var slope = this.getSlope(input);
-      console.log("\nThe slope of that equation is " + slope[0] + "/" + slope[1] + "\n");
+      const slope = this.getSlope(input);
+      console.log(`\nThe slope of that equation is ${slope[0]}/${slope[1]}\n`);
       this.drawLine(slope);
       console.log("Pretty cool line there!");
     }
